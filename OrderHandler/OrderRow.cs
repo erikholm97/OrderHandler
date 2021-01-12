0f01c26b81a4ebaec090baa75039993bea9b4f45
Erik Holm <erik.holm@netigrate.se>
2021-01-12 12:09:49 +0100

    Foreign keys on OrderRows

--- OrderHandler/OrderRow.cs
+++ OrderHandler/OrderRow.cs
@@ -8,11 +8,18 @@ namespace OrderHandler.Data
 {
     public class OrderRow
     {
+        [Required]
         public int Id { get; set; }
         [Required]
         public int RowNumber { get; set; }
+
+        [ForeignKey("Orders")]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
         [Required]
         public int ArticleAmount { get; set; }
+
         [Required]
         [ForeignKey("Articles")]
         public int ArticleId { get; set; }
