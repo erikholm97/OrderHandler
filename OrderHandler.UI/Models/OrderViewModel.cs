5b4c41fc29de26799e3d549385737e4e78b408e8
Erik Holm <erik.holm@netigrate.se>
2021-01-12 13:00:06 +0100

    Added OrderViewModel
    This is to be used for displaying order.

---
+++ OrderHandler.UI/Models/OrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderHandler.UI.Models
+{
+    public class OrderViewModel
+    {
+        //Todo create model for displaying order, orderrows, articles.
+    }
+}
