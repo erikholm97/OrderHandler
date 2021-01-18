bbe4f5da3709e49c984e027cfb5d426611c927da
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-01-18 22:05:14 +0100

    Update Article.cs

--- OrderHandler/Article.cs
+++ OrderHandler/Article.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Text;
 
 namespace OrderHandler.Data
 {
     public class Article
     {
-        [Required]
-        public int Id { get; set; }
-        [Required]
+            [Required]
+            public int Id { get; set; }
+            [Required]
             public int ArticleNumber { get; set; }
             [Required]
             public string ArticleName { get; set; }
@@ -26,6 +27,16 @@ namespace OrderHandler.Data
                 db.SaveChanges();
 
                 return articleToCreate.Id;
+            }
+        }
+
+        public Article GetIfArticleByNameExist(string articleName)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                var article = db.Articles.Where(x => x.ArticleName == articleName).FirstOrDefault();
+
+                return article;
             }
         }
     }
