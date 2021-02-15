dafd1a6c374ff7c7459c80080ca81bb5cfdae1aa
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-02-15 21:32:54 +0100

    Create IUnitOfWork.cs

---
+++ OrderHandler.Core/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderHandler.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IOrderRepository Orders { get; }
+        IOrderRowRepository Artists { get; }
+
+        IArticleRepository Articles { get; }
+        Task<int> CommitAsync();
+    }
+}
