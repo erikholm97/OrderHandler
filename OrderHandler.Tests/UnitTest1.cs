cdcd8eb73745e4b65bd54d5836a789b928eb044d
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-03-13 18:29:14 +0100

    Added Test Project For TDD

---
+++ OrderHandler.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrderHandler.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
