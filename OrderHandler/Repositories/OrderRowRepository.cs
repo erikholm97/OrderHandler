a0aa6de83e9292591b51fd620add805e8b91e99a
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-04-01 22:36:04 +0200

    Update OrderRowRepository.cs

--- OrderHandler/Repositories/OrderRowRepository.cs
+++ OrderHandler/Repositories/OrderRowRepository.cs
@@ -10,34 +10,33 @@ namespace OrderHandler.Data.Repositories
 {
     public class OrderRowRepository : Repository<OrderRow>, IOrderRowRepository
     {
-        private ApplicationDbContext _context;
-
+  
         public OrderRowRepository(ApplicationDbContext context)
            : base(context)
         { }
 
         public async Task<int> CreateOrderRow(OrderRow orderRowToCreate)
         {
-           await _context.OrderRows.AddAsync(orderRowToCreate);
+           await ApplicationDbContext.OrderRows.AddAsync(orderRowToCreate);
 
             return orderRowToCreate.Id;
         }
 
         public async Task DeleteOrderRowsByOrderId(int orderId)
         {
-            var orderrows = _context.OrderRows.Where(x => x.OrderId == orderId).ToList();
+            var orderrows = ApplicationDbContext.OrderRows.Where(x => x.OrderId == orderId).ToList();
 
-            _context.OrderRows.RemoveRange(orderrows);
+            ApplicationDbContext.OrderRows.RemoveRange(orderrows);
         }
 
         public async Task<List<OrderRow>> GetOrderRowsByArticleName(string articleName)
         {
-            return await _context.OrderRows.Include(x => x.Article).Where(x => x.Article.ArticleName == articleName).ToListAsync();
+            return await ApplicationDbContext.OrderRows.Include(x => x.Article).Where(x => x.Article.ArticleName == articleName).ToListAsync();
         }
 
         public async Task<List<OrderRow>> GetOrderRowsByOrderIdAsync(int orderId)
         {
-           return await _context.OrderRows.Where(x => x.OrderId == orderId).ToListAsync();
+           return await ApplicationDbContext.OrderRows.Where(x => x.OrderId == orderId).ToListAsync();
         }
 
         public async Task<List<OrderRow>> GetOrderRowsWithOrderByOrderIdAsync(int id)
