631848d9d3102d2eb59baa7c3e7c67851f814cea
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-07-14 22:13:24 +0200

    Added SignInManager

--- OrderHandler.UI/Controllers/AccountController.cs
+++ OrderHandler.UI/Controllers/AccountController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OrderHandler.UI.Models;
+using System.Threading.Tasks;
 
 namespace OrderHandler.UI.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<IdentityUser> userManager;
+
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                                    SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var userCreateResult = await userManager.CreateAsync(user, model.Password);
+
+                if (userCreateResult.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (var error in userCreateResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
     }
 }
