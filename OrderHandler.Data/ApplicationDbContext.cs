b876e5af822bfe29f692be1bf86c7ce825d1ea75
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-07-14 20:39:55 +0200

    Changes

---
+++ OrderHandler.Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OrderHandler.Core.Models;
+using System.Collections.Generic;
+
+namespace OrderHandler.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderRow> OrderRows { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Article>()
+            .HasIndex(a => a.ArticleNumber)
+            .IsUnique();
+
+            DataSeeder.SeedData(modelBuilder);
+        }
+    }
+}
