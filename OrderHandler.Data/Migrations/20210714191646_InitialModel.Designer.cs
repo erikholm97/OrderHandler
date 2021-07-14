be1ca7c921f5379ce9f292c150f7066c17bab975
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-07-14 21:19:56 +0200

    Add asproles

--- OrderHandler.Data/Migrations/20210714191646_InitialModel.Designer.cs
+++ OrderHandler.Data/Migrations/20210714191646_InitialModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrderHandler.Data;
 
 namespace OrderHandler.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210714191646_InitialModel")]
+    partial class InitialModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
