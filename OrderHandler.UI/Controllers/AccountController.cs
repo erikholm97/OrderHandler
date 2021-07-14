0fe8a5020d6f5b97031d085a299bb09e54b5910d
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-07-14 22:00:44 +0200

    Register View

---
+++ OrderHandler.UI/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderHandler.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
