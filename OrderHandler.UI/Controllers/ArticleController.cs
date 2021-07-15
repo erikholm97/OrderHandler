b8af998fc615cda3c6dbdb793950bade04d97c9e
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-07-15 16:27:05 +0200

    Update ArticleController.cs

--- OrderHandler.UI/Controllers/ArticleController.cs
+++ OrderHandler.UI/Controllers/ArticleController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderHandler.Data;
 using OrderHandler.UI.Models;
 using OrderHandler.Core.Models;
 using OrderHandler.Core.Services;
